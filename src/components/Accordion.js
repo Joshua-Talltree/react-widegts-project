e5b53d1c38ffbe6691530b078b189195959154b8
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-07-12 11:30:01 -0500

    feat: added useState hook to the accordion function

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const onTitleClick = (index) => {
+        setActiveIndex(index);
+    };
+
     const renderedItems = items.map((item, index) => {
         return (
             <React.Fragment key={item.title}>
                 <div
                     className="title active"
-                    onClick={() => console.log('Title clicked', index)}
+                    onClick={() => onTitleClick(index)}
                 >
-                    <i className="dropdown icon"></i>
+                    <i className="dropdown icon" />
                     {item.title}
                 </div>
                 <div className="content active">
@@ -21,6 +27,7 @@ const Accordion = ({ items }) => {
 
     return <div className="ui styled accordion">
         {renderedItems}
+        <h1>{activeIndex}</h1>
     </div>
 };
 
